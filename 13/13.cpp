0388fb5cad04331a09d1ea0d40b5ecd5096475fc
Ekoramadhan13 <enugraha411@gmail.com>
2024-05-04 13:07:24 +0700

    membuat bidang berikutnya dari simpul sebelumnya mengarah ke simpul baru

--- 13/13.cpp
+++ 13/13.cpp
@@ -57,6 +57,12 @@ void addNode()
 			current->prev = newNode; // step 6 make the previous field of the currennt node point to the new nod
 		}
 
+		if (previous != NULL)
+		{
+			previous->next = newNode; // step 7 make the next field of the previous node point to the new node
+		}
+
+
 }	
 
 	
